eb76961bc58fefdacb3674c7f92f544b9c303c75
348913953 <348913953@316R2038CHLZFX1.EDU.YRDSB.NET>
2016-05-31 12:03:05 -0400

    Added

---
+++ Octapad/src/Server.java
@@ -0,0 +1,17 @@
+
+public class Server {
+	private static Display display;
+	
+	public static void main(String[] args) {
+		display = new Display();
+		new Server().go();
+	}
+	
+	//https://github.com/ItsWillsWei/Octopad/invitations
+	
+	public void go()
+	{
+		
+	}
+
+}
